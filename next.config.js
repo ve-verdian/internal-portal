840ea80e9658b6a5ff639ff98054f93e754de4bd
ve-verdian <masukankatasandi2292@gmail.com>
2026-07-11 12:50:40 +0700

    Configure static export and GitHub Actions for Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+// Mendeteksi apakah sistem sedang di-build oleh GitHub Actions
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig = {
-  allowedDevOrigins: ['192.168.12.70', 'localhost'],
+  // Hanya gunakan mode export dan basePath jika sedang di-build di GitHub
+  output: isGithubActions ? 'export' : undefined,
+  basePath: isGithubActions ? '/internal-portal' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
