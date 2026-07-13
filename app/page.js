2c0722b254ef9863897434b1b929eb743aa5e1e6
ve-verdian <masukankatasandi2292@gmail.com>
2026-07-13 12:02:50 +0700

    add another grafana

--- app/page.js
+++ app/page.js
@@ -29,7 +29,8 @@ export default function Home() {
       category: "📊 Monitoring & Server Health",
       services: [
         { name: "Zabbix", desc: "Enterprise-Class Network & Server Monitoring", url: "http://192.168.12.24/zabbix", icon: "📡" },
-        { name: "Grafana", desc: "Visualisasi Data & Dashboard Analitik", url: "http://192.168.12.24:3000/d/bpjs-monitoring-v4/monitoring-api-bpjs?kiosk", icon: "📈" },
+        { name: "Grafana - Ubuntu", desc: "Visualisasi Data & Dashboard Analitik", url: "http://192.168.12.24:3000/d/bpjs-monitoring-v4/monitoring-api-bpjs?kiosk", icon: "📈" },
+        { name: "Grafana - Debian", desc: "Visualisasi Data & Dashboard Analitik", url: "http://192.168.12.29:3000/", icon: "📈" },
         { name: "Prometheus", desc: "Sistem Monitoring & Time-Series Database", url: "http://192.168.12.24:9090/service-discovery", icon: "🟢" },
         { name: "Uptime Kuma", desc: "Monitoring Status Uptime Server", url: "http://192.168.12.5:3001/status/server-monitoring", icon: "❤️" },
         { name: "Uptime Kuma", desc: "Monitoring Status Uptime ISP", url: 
